7741567230d00db49a86ae1ea04ae627b5761b94
CodeFluctus <utte123@gmail.com>
2018-10-03 14:45:33 +0300

    Lisätty vammainen tabulaattori

--- dbInsertUusiSuoritus.php
+++ dbInsertUusiSuoritus.php
@@ -1,33 +1,33 @@
 <?php
 
-$host = 'localhost';
-$dbname = 't7aaju00';
-$username = 't7aaju00';
-$password = 'salasana';
+    $host = 'localhost';
+    $dbname = 't7aaju00';
+    $username = 't7aaju00';
+    $password = 'salasana';
 
-$con = myslqi_connect($host, $username, $password, $dbname);
+    $con = myslqi_connect($host, $username, $password, $dbname);
 
-if (myslqi_connect_errno())
-{
-    echo "Failed to connect MySQL! Error: " . myslqi_connect_error(); 
-}
+    if (myslqi_connect_errno())
+    {
+        echo "Failed to connect MySQL! Error: " . myslqi_connect_error(); 
+    }
 
-$etunimi = mysqli_real_escape_string($con, filter_input(INPUT_POST,'etunimi',FILTER_SANITIZE_STRING));
-$sukunimi = mysqli_real_escape_string($con, filter_input(INPUT_POST,'sukunimi',FILTER_SANITIZE_STRING));
-$opintojaksonKoodi = mysqli_real_escape_string($con,filter_input(INPUT_POST,'ojkoodi',FILTER_SANITIZE_STRING));
-$arvosana = mysqli_real_escape_string($con, filter_input(INPUT_POST,'arvosana',FILTER_SANITIZE_STRING));
+    $etunimi = mysqli_real_escape_string($con, filter_input(INPUT_POST,'etunimi',FILTER_SANITIZE_STRING));
+    $sukunimi = mysqli_real_escape_string($con, filter_input(INPUT_POST,'sukunimi',FILTER_SANITIZE_STRING));
+    $opintojaksonKoodi = mysqli_real_escape_string($con,filter_input(INPUT_POST,'ojkoodi',FILTER_SANITIZE_STRING));
+    $arvosana = mysqli_real_escape_string($con, filter_input(INPUT_POST,'arvosana',FILTER_SANITIZE_STRING));
 
-$sql = "CALL UusiSuoritus('$etunimi', '$sukunimi', '$opintojaksonKoodi', '$arvosana')";
+    $sql = "CALL UusiSuoritus('$etunimi', '$sukunimi', '$opintojaksonKoodi', '$arvosana')";
 
-if (!mysqli_query($con, $sql))
-{
-    die('Error: ' . mysqli_error($con));
-}
-    
-echo "Merkintä tietokantaan suoritettu onnistuneesti!";
-echo "<br>";
-echo "Palaa selaimen edellinen-painikkeella takaisin!";
+    if (!mysqli_query($con, $sql))
+    {
+        die('Error: ' . mysqli_error($con));
+    }
+        
+    echo "Merkintä tietokantaan suoritettu onnistuneesti!";
+    echo "<br>";
+    echo "Palaa selaimen edellinen-painikkeella takaisin!";
 
-mysqli_close($con);
+    mysqli_close($con);
 
 ?>
