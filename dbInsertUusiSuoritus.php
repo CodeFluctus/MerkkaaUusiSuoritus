aa2bffc965708d84c11a4da9d99fafa3ff9bbfb3
CodeFluctus <utte123@gmail.com>
2018-10-03 12:37:53 +0300

    Luotut php-tiedosto. Luo yhteyden tietokantaan ja lisää sinne opiskelijalle arvosanan kyseiselle opintojaksolle.

---
+++ dbInsertUusiSuoritus.php
@@ -0,0 +1,33 @@
+<?php
+
+$host = 'localhost';
+$dbname = 't7aaju00';
+$username = 't7aaju00';
+$password = 'salasana';
+
+$con = myslqi_connect($host, $username, $password, $dbname)
+
+if (myslqi_connect_errno())
+{
+    echo "Failed to connect MySQL! Error: " . myslqi_connect_error(); 
+}
+
+$etunimi = mysqli_real_escape_string($con, $_POST['etunimi']);
+$sukunimi = mysqli_real_escape_string($con, $_POST['sukunimi']);;
+$opintojaksonNimi = mysqli_real_escape_string($con, $_POST['ojnimi']);
+$arvosana = mysqli_real_escape_string($con, $_POST['arvosana']);
+
+$sql = "CALL PROCEDURE UusiSuoritus('$etunimi', '$sukunimi', '$opintojaksonNimi', '$arvosana')";
+
+if (!mysqli_query($con, $sql))
+{
+    die('Error: ' . mysqli_error($con));
+}
+    
+echo "Merkintä tietokantaan suoritettu onnistuneesti!";
+echo "<br>";
+echo "Palaa selaimen edellinen-painikkeella takaisin!";
+
+mysqli_close($con);
+
+?>
